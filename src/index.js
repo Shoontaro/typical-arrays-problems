f8b62c176ca0361d8508b37536230ab86cbc0e78
Shoon <tishinadw@gmail.com>
2021-02-22 22:10:20 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array)) {
+    return 0;
+}
+
+if (array.length === 0) {
+    return 0;
+}
+  else{return Math.min.apply(null, array)};
+
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!Array.isArray(array)) {
+    return 0;
+}
+
+if (array.length === 0) {
+    return 0;
+}
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array)) {
+    return 0;
+}
+
+if (array.length === 0) {
+    return 0;
+}
+  let sum = array.reduce(function(a,b){return(a+b)});
+  return sum/array.length;
 }
